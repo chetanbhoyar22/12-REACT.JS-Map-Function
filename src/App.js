177f3b5c27f5962eaa3d49281daa9f3e8165af87
chetanbhoyar22 <cab9722@gmail.com>
2021-02-14 21:48:50 +0530

    REACT.JS-Map-Function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import  './Chetan.css';
+import Extra from './Extra.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App()
+{
+  const allowedState = 
+  [
+    { MyCar: "BMW", Owner : "ABC" },
+    { MyCar: "TESLA", Owner : "ELON " },
+    { MyCar: "TATA", Owner : "RATAN TATA " },
+   
+  ];
+
+  return(
+    <div>
+      <h2>Select Tag (Dropdown) -</h2>
+
+      <select>
+              {allowedState.map((i, index) => (
+                <option key={i.id}>{i.MyCar}</option>
+              ))}
+      </select>
+     
+      <h2>ol Tag (Ordered List) -</h2>
+
+      <ol>
+              {allowedState.map((i, index) => (
+                <li key={i.id}>{i.MyCar}</li>
+              ))}
+      </ol>
+
+      <h2>ul Tag (Unordered List) -</h2>
+
+      <ul>
+              {allowedState.map((i, index) => (
+                <li >{i.MyCar}</li>
+              ))}
+      </ul>
+
+      <h2>Table Tag -</h2>
+
+      <table className="MyTable">
+                <tr>
+                   
+                    <td>Sr. No.</td>
+                    <td>Name</td>
+                    <td>Owner Name</td>
+                </tr>
+                
+                {allowedState.map((i, index) => (
+                  <tr>
+                <td key={i.id}>{index+1}</td>
+                <td key={i.id}>{i.MyCar}</td>
+                <td key={i.id}>{i.Owner}</td>
+                </tr>
+              ))}  
+      </table>
+      <Extra/>
     </div>
   );
 }
-
 export default App;
---
+++ src/Extra.js
@@ -0,0 +1,11 @@
+function Extra()
+{
+    return(
+            <div>
+                <p>This is the paragraph tag..!</p>
+            </div>
+
+    );
+
+}
+export default Extra
